4ac92889fbbc96d548a21611bef85376e48c9a46
Michiel Van den Berghe <michiel.vdb@gmail.com>
2015-03-30 22:53:19 +0200

    Fix faulty detection

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'ja' if sphere.lights_on() else 'nee'
+    return 'ja' if sphere.lights_on() else 'waarschijnlijk niet'
 
 if __name__ == '__main__':
     app.run()
